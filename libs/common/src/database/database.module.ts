b6ca77ba30a01a04d817bc6b32ce958483b61faf
Zain Gulbaz <zain.gulbaz@vetdrive.co>
2024-02-06 16:06:39 +0500

    Added Configuration and database modules in commons library

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '../config/config.module';
 
 @Module({
-    imports:[MongooseModule.forRoot("mongodb://localhost:27017/sleepr")]
+    imports:[MongooseModule.forRootAsync({
+        imports: [ConfigModule],
+        useFactory: async (configService: ConfigService) => ({
+          uri: configService.get<string>('MONGODB_URI'),
+        }),
+        inject: [ConfigService],
+      })]
 })
 export class DatabaseModule {}
